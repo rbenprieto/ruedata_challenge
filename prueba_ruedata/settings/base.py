68b1036fad3144db09d8ebe32bc6a4ee71ae63f9
rbenprieto <rbenprieto@outlook.com>
2023-06-05 04:17:41 -0500

    statics

--- prueba_ruedata/settings/base.py
+++ prueba_ruedata/settings/base.py
@@ -97,8 +97,8 @@ TIME_ZONE = "America/Bogota"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
