9e3c4224a8a9b54b608f18959bbd3d05de7717a9
rbenprieto <rbenprieto@outlook.com>
2023-06-05 03:52:37 -0500

    util core

---
+++ apps/prueba/utils.py
@@ -0,0 +1,50 @@
+from typing import List, Dict, Any
+from collections import deque
+
+
+def find_shortest_possible_key(attempts_login: List[str]) -> str:
+    """
+    Function to perform the validations and generate the code recursively
+    """
+
+    # Diccionarios para apendear data
+    graph: Dict[str, List[str]] = {}
+    in_degree: Dict[str, int] = {}
+
+    # Iteracion sobre cada intento de login
+    for attempt in attempts_login:
+
+        # Iteracion sobre el index de cada intento de login
+        for i in range(len(attempt) - 1):
+
+            # Obtiene el número del actual index y el proximo
+            node = attempt[i]
+            adjacent_node = attempt[i + 1]
+
+            #Evalua si el nodo no está en el grafo y lo asigna de ser necesario
+            if node not in graph:
+                graph[node] = []
+                in_degree[node] = 0
+
+            #Verifica que el nodo del proximo indice sea una relación del actual indice, se asigna como conexión y aumenta la cantidad de nodos adjacentes
+            if adjacent_node not in graph[node]:
+                graph[node].append(adjacent_node)
+                in_degree[adjacent_node] = in_degree.get(adjacent_node, 0) + 1
+
+    #Genera una lista de los nodos que no tengan conexiones de entrada
+    node_list = [node for node in graph if in_degree[node] == 0]
+    sorted_result = []
+
+    while node_list:
+        # Sacar el primer nodo de la lista para appendear
+        node = node_list.pop(0)
+        sorted_result.append(node)
+
+        # Actualizar el grado de entrada de los nodos adyacentes y si es cero appendearlo a la lista
+        for adjacent_node in graph.get(node, []):
+            in_degree[adjacent_node] -= 1
+            if in_degree[adjacent_node] == 0:
+                node_list.append(adjacent_node)
+
+    #Concatena el resultado y lo convierte a entero
+    return int("".join(sorted_result))
--- apps/prueba/views.py
+++ apps/prueba/views.py
@@ -15,27 +15,44 @@ class FileUploadView(APIView):
     """
     View to process the text file and generate the expected response
     """
+
     parser_class = FileUploadParser
 
     def post(self, request, *args, **kwargs):
         file = request.FILES.get("keylog")
 
         if file:
-            #Validation for the type of file
+            #Valida el tipo de archivo
             if not file.name.lower().endswith(".txt"):
                 raise ValidationError({"error": "This file must be a txt file (.txt)"})
 
-            #Select the file and generate randomly selected elements
+            # Selecciona el archivo, lo lee, decodifica y genera una selección aleatoria como muestra
             lines: List[str] = file.read().decode("utf-8").splitlines()
             len_lines: int = len(lines)
             amount_random: int = random.randint(1, len_lines)
             selected_items: List[str] = random.choices(lines, k=amount_random)
 
-            
-            return Response({
-                "secret_code": "key",
-                "amount_data_sample": amount_random,
-                "data_sample": selected_items 
-            }, status=status.HTTP_200_OK)
+            # Validar la cantidad de caracteres de cada intento de login
+            selected_items_wrong_format = [
+                item for item in selected_items if len(item) != 3
+            ]
+            if selected_items_wrong_format:
+                raise ValidationError(
+                    {
+                        "error": "The attemps have a format invalid, must be 3 characters",
+                        "attempts_invalid": selected_items_wrong_format,
+                    }
+                )
+
+            #Usa el útil de forma recursiva para obtener el key de login más corto posible
+            key: str = find_shortest_possible_key(selected_items)
+            return Response(
+                {
+                    "secret_code": key,
+                    "amount_data_sample": amount_random,
+                    "data_sample": selected_items,
+                },
+                status=status.HTTP_200_OK,
+            )
 
         raise ValidationError({"error": "The file was not provided"})
